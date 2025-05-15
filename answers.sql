a7d106f1f06b502235e7af3e322a076003bf4158
cj-lorakosta <kiarahojames65@gmail.com>
2025-05-15 09:42:56 +0300

    save my work

---
+++ answers.sql
@@ -0,0 +1,34 @@
+
+--Question 1
+SELECT
+  OrderID,
+  CustomerName,
+  TRIM(value) AS Product
+FROM
+  ProductDetail
+  CROSS APPLY STRING_SPLIT(Products, ',');
+
+--Question 2
+     step 1:
+
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+
+CREATE TABLE OrderItems (
+  OrderID INT,
+  Product VARCHAR(100),
+  Quantity INT,
+  PRIMARY KEY (OrderID, Product)
+);
+ 
+   srep 2:
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
